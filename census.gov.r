d3513efc9ff8c0f3cd7a170e43802b77f5cdda2c
Adam Sampson <sampson.adam@outlook.com>
2017-04-21 18:32:29 -0400

    Initial Files

---
+++ census.gov.r
@@ -0,0 +1,29 @@
+##################################################
+## US Census
+## https://www.census.gov/data/developers/data-sets.html
+## API_key <- 47b9ccb0ef62093f07d29c7b7c5d7cf099324f90
+##################################################
+
+## American Community Survey 5-year Data
+# https://www.census.gov/data/developers/data-sets/acs-5year.html
+# 2009 - 2015
+
+## American Community Survey 3-year Data
+# https://www.census.gov/data/developers/data-sets/acs-3year.html
+# 2012 - 2013
+
+## American Community Survey 1-year Data
+# https://www.census.gov/data/developers/data-sets/acs-1year.html
+# 2011 - 2015
+
+## American Community Survey Migration Flows
+# https://www.census.gov/data/developers/data-sets/acs-migration-flows.html
+# For dates see link.
+
+## Annual Survey of Manufacturers
+# https://www.census.gov/data/developers/data-sets/Annual-Survey-of-Manufactures.html
+# 2002-2015
+
+## 2012 Survey of Business Owners
+# https://www.census.gov/data/developers/data-sets/business-owners.html
+# 2012
---
+++ ucrdatatool.gov.r
@@ -0,0 +1,14 @@
+########################################
+## Uniform Crime Reporting Statistics
+## Can't find an API? 
+## Requires webscraping then?
+## https://www.ucrdatatool.gov/
+########################################
+
+## An example pretty output is found at: https://www.socialexplorer.com/18f4fa9c60/explore
+
+## Questions that could be asked:
+# Are non-violent crimes related to population density?
+# Are violent crimes related to population density?
+# Do crime rates (per 100k people) change as the population increases?
+# Etc. 
