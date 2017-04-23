0a41f82285fc15cf8b4d92d819298c9d4cb2a8d0
Adam Sampson <caver.adam@outlook.com>
2017-04-23 10:13:20 -0400

    Added blsAPI

--- bls.gov.r
+++ bls.gov.r
@@ -4,15 +4,22 @@
 ########################################
 
 
-https://api.bls.gov/publicAPI/v2/timeseries/data/
-  
+#https://api.bls.gov/publicAPI/v2/timeseries/data/
+install.packages("blsAPI")  
 library(RCurl)
 library(tidyverse)
 library(jsonlite)
+library(rvest)
+library(blsAPI)
 
-#bls <- fromJSON("https://api.bls.gov/publicAPI/v2/timeseries/data/SMU19197802023800001")
+## Originally we did:
 bls <- fromJSON("https://api.bls.gov/publicAPI/v2/timeseries/data/SMU19197802023800001")
 bls2 <- bls[4]
 bls3 <- as.data.frame(bls2)
 bls4 <- as.data.frame(bls3[2])
 bls5 <- matrix(unlist(bls4),ncol=27,byrow=TRUE)
+
+## Easy way
+payload <- 'SMU19197802023800001'
+response <- blsAPI(payload,return.data.frame = TRUE)
+json <- fromJSON(response)
